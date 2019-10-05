2c4afa953be41303e12a3318c2b23aa4111af2a3
Giovani Garfias <giovaniportal@gmail.com>
2019-10-04 20:37:10 -0700

    Finished closure.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -4,6 +4,15 @@
 // that manipulates variables defined in the outer scope.
 // The outer scope can be a parent function, or the top level of the script.
 
+function violenceCA(){
+  const violenceTracker = "High";
+  console.log(`The violence in California is ${violenceTracker}!`);
+}
+  function cityTracker(){
+    const cityName = "Oakland"
+    console.log(`The violence in ${cityName} is ${violenceTracker}!`);
+}
+
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
